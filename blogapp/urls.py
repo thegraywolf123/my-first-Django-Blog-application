2cdc20ebd3391d240a317a39a75a177d2f71f0b6
thegraywolf <agborrichard123@gamil.com>
2023-01-09 01:53:50 -0800

    updated css

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,5 +1,5 @@
-from xml.dom.minidom import Document
-from django.urls import path
+from django.urls import path, include
+# from .views import index, home_api, post_single
 from .import views
 from blogproject.settings import DEBUG, STATIC_URL, STATICFILES_DIR, MEDIA_URL,MEDIA_ROOT
 from django.conf import settings
@@ -10,9 +10,11 @@ app_name = 'blog'
 
 urlpatterns = [
     
-    path('', views.home, name='homepage'),
-    path('<slug:post>/', views.post_single, name='post_single'),
+    path('', views.index, name ='index'),
+    path('home/', views.home,name = 'home'),
+    path('<slug:post>/', views.post_single, name ='post_single'),
 ]
+
 if DEBUG:
     urlpatterns += static(STATIC_URL, document_root = STATICFILES_DIR)
     urlpatterns += static(MEDIA_URL, document_root = MEDIA_ROOT )
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,13 +1,34 @@
+from unicodedata import category
+from urllib import response
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+import requests
+import json
+API_KEY ='0e9468e8f6364465befd5ed4809bd80a'
 
 # Create your views here.
 
-def home(request):
+def index(request):
     # all_posts = Post.newmanager.all()
     all_posts = Post.objects.all()
-    return render(request,'index.html',{'posts' : all_posts })
+    return render(request,'index.html', {'posts' : all_posts })
 
 def post_single(request, post):
     post = get_object_or_404(Post, slug=post, status='published')
     return render(request, 'single.html', {'post': post})
+
+def home(request):
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data ['articles']
+    else:
+        url =f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data ['articles']
+    return render(request, 'home.html',{'articles':articles})
